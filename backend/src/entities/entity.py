ba17ad1c1e13d93a139610aac79e7207c2c46362
Tim B <timbo95+github@posteo.de>
2020-04-18 22:19:02 +0200

    add extension für db_config.json

---
+++ backend/src/entities/entity.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
