bf9729b8751c2dec586f982bfa03cad1546009b9
Tim B <timbo95+github@posteo.de>
2020-04-19 16:32:15 +0200

    implement methods for Flask Application in main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,27 +1,47 @@
 # coding=utf-8
 
+from flask import Flask, jsonify, request
+
 from backend.src.entities.entity import Session, engine, Base
-from backend.src.entities.exam import Exam
+from backend.src.entities.exam import Exam, ExamSchema
+
+# create the Flask application
+app = Flask(__name__)
 
 # generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-exams = session.query(Exam).all()
+@app.route('/exams')
+def get_exams():
 
-if len(exams) == 0:
-    # create and persist dummy exam
-    python_exam = Exam("SQLAlchemy Exam", "Test your knowledge about SQLAlchemy", "script")
-    session.add(python_exam)
-    session.commit()
+    # fetching from the database
+    session = Session()
+    exam_objects = session.query(Exam).all()
+
+    # transform into JSON-serializable objects
+    schema = ExamSchema(many=True)
+    exams = schema.dump(exam_objects)
+
+    # serialize as JSON
     session.close()
+    return jsonify(exams.data)
 
-    exams = session.query(Exam).all()
 
-# show existing exams
-print('### Exams:')
-for exam in exams:
-    print(f'({exam.id}) {exam.title} - {exam.description}')
+@app.route('/exams', methods=['POST'])
+def add_exam():
+
+    # mount exam object
+    posted_exam = ExamSchema(only=('title', 'description')).load(request.get_json())
+
+    exam = Exam(**posted_exam.data, created_by="HTTP post request")
+
+    # persist exam
+    session = Session()
+    session.add(exam)
+    session.commit()
+
+    # return created exam
+    new_exam = ExamSchema().dump(exam).data
+    session.close()
+    return jsonify(new_exam), 201
