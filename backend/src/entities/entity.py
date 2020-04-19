3b9a4544a0ee46db05e51c2e70108f5792a9cf2c
Tim B <timbo95+github@posteo.de>
2020-04-19 09:07:12 +0200

    implement entity class

--- backend/src/entities/entity.py
+++ backend/src/entities/entity.py
@@ -22,3 +22,16 @@ Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
+
+class Entity():
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    last_updated_at = Column(String)
+
+    def __init__(self, created_by):
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.last_updated_at = created_by
+
